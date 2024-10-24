17a49b1e8009c6b7612194cca774b7fffe3ad80d
elchukc <clareelchuk@gmail.com>
2024-10-23 23:21:49 -0400

    vite renovation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <nav className="flex gap-2">
+        <a href="https://github.com/elchukc" target="_blank">
+          Github
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="src/assets/resume.pdf" target="_blank">
+          Resume
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+
+      </nav>
+      <h1>This site is currently under construction</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,7 +26,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
