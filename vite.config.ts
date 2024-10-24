d8cdacdf82666a3caf04879ebd135c56258a18b3
elchukc <clareelchuk@gmail.com>
2024-10-24 13:48:06 -0400

    and svg

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['~/assets/*.pdf']
+  assetsInclude: [
+    '**/assets/*.pdf',
+    '**/assets/*.svg',
+  ]
 })
